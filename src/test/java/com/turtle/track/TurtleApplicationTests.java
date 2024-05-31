3f197ada688be64eb3e8ac509e6d0445a3340654
mari <mariana.msabio@gmail.com>
2024-05-31 13:12:50 -0300

    First Commit

---
+++ src/test/java/com/turtle/track/TurtleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
